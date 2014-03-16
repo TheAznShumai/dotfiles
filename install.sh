2304463f0bbb243750a5ec2316196e0a6daf5edf
Ariya Seng <natsumi@gmail.com>
2014-03-15 18:47:03 -0700

    tmux install update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir -p ~/dev/.virtualenv
-mkdir -p ~/.vim/autoload 
+mkdir -p ~/.vim/autoload
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
@@ -13,3 +13,4 @@ ln -s $PWD/bash/bash-it/ ~/.bash_it
 ln -s $PWD/bash/.bash_profile ~/
 ln -s $PWD/rails/.gemrc ~/
 ln -s $PWD/rails/.pryrc ~/
+ln -s $PWD/tmux/.tmux.conf ~/
